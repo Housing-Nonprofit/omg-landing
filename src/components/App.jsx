2b6ae9d377d600e6efc1fa9bcb1ea1e8684d1539
Luis fernando Marquez gomez <luisfernandomarquezgomez@Luiss-MacBook-Pro.local>
2025-04-25 15:55:49 -0500

    feat: daisy ui added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <h1>Hello world</h1>
+      <p>Welcome to my React app!</p>
+    </BrowserRouter>
     </>
   )
 }
