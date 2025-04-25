3dd269782a9960ebf5738e421d2d59afb7b55b45
Luis fernando Marquez gomez <luisfernandomarquezgomez@Luiss-MacBook-Pro.local>
2025-04-25 16:24:46 -0500

    fix: daisy theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
+import '../index.css'
 function App() {
 
   return (
@@ -6,6 +7,14 @@ function App() {
     <BrowserRouter>
       <h1>Hello world</h1>
       <p>Welcome to my React app!</p>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </BrowserRouter>
     </>
   )
